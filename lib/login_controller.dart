d0509d736aeeee896a8bd27a74d8079b3a9f52ab
Prajeeth347 <prajeeth347@gmail.com>
2021-10-30 16:36:08 +0530

    Google stay signed in bug fix

--- lib/login_controller.dart
+++ lib/login_controller.dart
@@ -5,13 +5,21 @@ import 'package:get/get.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:hackathon/googleloginmain.dart';
 import 'package:hackathon/main.dart';
+import 'package:hackathon/signinpage.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginController extends GetxController {
   var _googleSignIn = GoogleSignIn();
   var googleAccount = Rx<GoogleSignInAccount?>(null);
   authentication() async {
-    googleAccount.value = await _googleSignIn.signIn();
+    if (_googleSignIn.isSignedIn() == true) {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      prefs.remove('gmail');
+      return loginpage();
+    } else {
+      googleAccount.value = await _googleSignIn.signIn();
+      return signinpage();
+    }
   }
 
   login(BuildContext context) async {

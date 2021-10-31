2620851e0ee7971c55a0078c08e0c6048bf8c832
Prajeeth347 <prajeeth347@gmail.com>
2021-10-31 15:42:06 +0530

    final reviews and ratings

--- lib/collegereviews.dart
+++ lib/collegereviews.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
@@ -39,7 +40,6 @@ class Userinfo extends StatefulWidget {
 }
 
 class _UserinfoState extends State<Userinfo> {
-  var ratingslength;
   final Stream<QuerySnapshot> _collegesstream = FirebaseFirestore.instance
       .collection(collegeratings.collegename)
       .snapshots();
@@ -56,66 +56,70 @@ class _UserinfoState extends State<Userinfo> {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return Center(child: CircularProgressIndicator());
           }
-          ratingslength = snapshot.data?.size;
           return ListView(
             children: snapshot.data!.docs.map((DocumentSnapshot document) {
               Map<String, dynamic> data =
                   document.data()! as Map<String, dynamic>;
-              return ListTile(
-                title: Text(
-                  data['Title'],
-                  style: TextStyle(color: Colors.white, fontSize: 20),
-                ),
-                leading: Icon(
-                  Icons.account_circle,
-                  color: Colors.white,
-                ),
-                subtitle: Align(
-                  alignment: Alignment.topLeft,
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(top: 16.0, bottom: 16.0),
-                        child: Align(
-                          alignment: Alignment.topLeft,
-                          child: RatingBarIndicator(
-                            unratedColor: Colors.grey.shade900,
-                            itemSize: 20,
-                            rating: data['rating'],
-                            itemBuilder: (context, index) => Icon(
-                              Icons.star,
-                              color: Colors.white,
+              return Column(
+                children: [
+                  ListTile(
+                    title: Text(
+                      data['Title'],
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                    leading: Icon(
+                      Icons.account_circle,
+                      color: Colors.white,
+                    ),
+                    subtitle: Align(
+                      alignment: Alignment.topLeft,
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding:
+                                const EdgeInsets.only(top: 16.0, bottom: 16.0),
+                            child: Align(
+                              alignment: Alignment.topLeft,
+                              child: RatingBarIndicator(
+                                unratedColor: Colors.grey.shade900,
+                                itemSize: 20,
+                                rating: data['rating'],
+                                itemBuilder: (context, index) => Icon(
+                                  Icons.star,
+                                  color: Colors.white,
+                                ),
+                              ),
                             ),
                           ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 16.0),
-                        child: Align(
-                          alignment: Alignment.topLeft,
-                          child: Text(
-                            data['Email'],
-                            style: TextStyle(
-                              color: Colors.white,
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: 16.0),
+                            child: Align(
+                              alignment: Alignment.topLeft,
+                              child: Text(
+                                data['Email'],
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
                             ),
                           ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 16.0),
-                        child: Align(
-                          alignment: Alignment.topLeft,
-                          child: Text(
-                            data['Body'],
-                            style: TextStyle(
-                              color: Colors.white,
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: 16.0),
+                            child: Align(
+                              alignment: Alignment.topLeft,
+                              child: Text(
+                                data['Body'],
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
                             ),
                           ),
-                        ),
+                        ],
                       ),
-                    ],
+                    ),
                   ),
-                ),
+                ],
               );
             }).toList(),
           );
